0bb8a0df09dcdd11a71aa812cc616f27a32a407e
Pranav2612000 <pranav2000joglekar@gmail.com>
2026-05-06 05:19:31 +0000

    feat build: update api version and search path for linux

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
 fn main() {
     // This assumes the library is in a known location, e.g., /usr/local/lib
     // or defined by an FDB_LIB_DIR environment variable
-    println!("{}", format!("-D FDB_API_VERSION=630"));
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    println!("{}", format!("-D FDB_API_VERSION=730"));
+    println!("cargo:rustc-link-search=/usr/lib");
     println!("cargo:rustc-link-lib=fdb_c");
 }
-
