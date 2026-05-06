09aceaa089fc857b3c2b456d47423efb9454d272
Pranav Joglekar <pranav2000joglekar@gmail.com>
2026-05-06 12:55:00 +0530

    feat: add support for conditional build differences between linux and mac

--- build.rs
+++ build.rs
@@ -2,6 +2,14 @@ fn main() {
     // This assumes the library is in a known location, e.g., /usr/local/lib
     // or defined by an FDB_LIB_DIR environment variable
     println!("{}", format!("-D FDB_API_VERSION=730"));
-    println!("cargo:rustc-link-search=/usr/lib");
+    #[cfg(target_os = "linux")]
+    {
+        println!("cargo:rustc-link-search=/usr/lib");
+    }
+    #[cfg(not(target_os = "linux"))]
+    {
+        println!("cargo:rustc-link-search=/usr/local/lib");
+    }
+
     println!("cargo:rustc-link-lib=fdb_c");
 }
